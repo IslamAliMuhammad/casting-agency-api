993cec13738ed42e546cabff4488cea625bc9ea8
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-03-28 12:02:51 +0200

    test: Test for error behavior and Test for roles (unit test)

--- test_app.py
+++ test_app.py
@@ -42,7 +42,11 @@ class CastingAgencyTestCase(unittest.TestCase):
 
     def header(self, token):
         return {'Content-Type': 'application/json', 'Authorization': 'bearer ' + token}
-    # POST
+    
+    ###### Test endpoints for success and error behavior
+
+    # POST 
+    ## Success behavior
     def test_create_movie(self):
         self.client().post('/movies', headers=self.header(self.executive_producer), json=self.new_movie)
         res = self.client().post('/movies', headers=self.header(self.executive_producer), json=self.new_movie)
@@ -50,6 +54,7 @@ class CastingAgencyTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+
 
     def test_create_actor(self):
         self.client().post('/actors', headers=self.header(self.executive_producer), json=self.new_actor)
@@ -60,7 +65,25 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    ## Error behavior
+    def test_401_post_movie_without_auth_header(self):
+        res = self.client().post('/movies', json=self.new_movie)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+    
+    def test_401_post_actor_without_auth_header(self):
+        res = self.client().post('/actors', json=self.new_movie)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    ###----------------------------------------------------------------------------------------------------------
     # GET
+    ## Success behavior
+
     def test_get_movies(self):
         res = self.client().get('/movies', headers=self.header(self.casting_assistant))
         data = json.loads(res.data)
@@ -68,6 +91,7 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['movies'])
+        
 
     def test_get_actors(self):
         res = self.client().get('/actors', headers=self.header(self.casting_assistant))
@@ -77,7 +101,23 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    ## Error behavior
+    def test_401_get_movies_without_auth_header(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    def test_401_get_actors_without_auth_header(self):
+        res = self.client().get('/actors')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+    ###----------------------------------------------------------------------------------------------------------
     # # PATCH
+    ## Success behavior
     def test_update_movie_partially(self):
         res = self.client().patch('/movies/1', headers=self.header(self.executive_producer), json={'title': 'new-title'})
         data = json.loads(res.data)
@@ -92,8 +132,25 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    
+    # Error behavior
+    def test_401_update_movie_partially_without_auth_header(self):
+        res = self.client().patch('/movies/1', json={'title': 'new-title'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    def test_401_update_actor_partially_without_auth_header(self):
+        res = self.client().patch('/actors/1', json={'title': 'new-title'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    ###----------------------------------------------------------------------------------------------------------
     # DELETE
+    ## Success behavior
+
     def test_delete_movie(self):
         res = self.client().delete('/movies/2', headers=self.header(self.executive_producer))
         data = json.loads(res.data)
@@ -104,6 +161,59 @@ class CastingAgencyTestCase(unittest.TestCase):
     def test_delete_actors(self):
         res = self.client().delete('/actors/2', headers=self.header(self.executive_producer))
         data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    ## Error behavior
+    def test_401_delete_movie_without_auth_header(self):
+        res = self.client().delete('/movies/2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    def test_401_delete_actors_without_auth_header(self):
+        res = self.client().delete('/actors/2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    #####---------------------------------------------------------------------------------------#####
+
+    # Test role-based access control
+
+    ## EXECUTIVE PRODUCER 
+    def test_get_movies_with_executive_producer(self):
+        res = self.client().get('/movies', headers=self.header(self.executive_producer))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movies'])
+
+    def test_get_actors_with_executive_producer(self):
+        res = self.client().get('/actors', headers=self.header(self.executive_producer))
+        data = json.loads(res.data)
+        self.assertTrue(data['actors'])
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    ## CASTING ASSISTANT
+    def test_get_movies_with_casting_assistant(self):
+        res = self.client().get('/movies', headers=self.header(self.casting_assistant))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movies'])
+
+    def test_get_actors_with_casting_assistant(self):
+        res = self.client().get('/actors', headers=self.header(self.casting_assistant))
+        data = json.loads(res.data)
+        self.assertTrue(data['actors'])
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
